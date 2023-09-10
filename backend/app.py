756c0b711804f2ff1eaed250b406576a7dc6767b
bitmaxx07 <84411099+bitmaxx07@users.noreply.github.com>
2023-09-10 21:42:05 +0800

    more test lines...

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -14,21 +14,32 @@ def index():
 def upload_xml():
     try:
         # print(request)
-        xml_data = request.files['xmlData'].read()
+        print("Reading xml data")
+        if 'xmlData' in request.files:
+            xml_data = request.files['xmlData'].read()
+        else:
+            xml_data = request.data
         # print(xml_data)
         print("XML Data Received:")
         print(xml_data.decode())
         return jsonify({"message": "XML data received successfully"})
+
     except Exception as e:
         print(e)
-        return jsonify({"error": str(e)})
+        print(request.headers)
+        print(request.data)
+        '''print(request.content_type)
+        return request.data'''
+        error_response = make_response("Error: " + str(e), 400)
+        error_response.headers['Content-Type'] = 'text/plain'
+        return error_response
 
 
 @app.route('/get-xml', methods=['POST'])
 def get_xml_data():
     xml_data = request.data
     app.logger.info("Received XML data: %s", xml_data)
-    
+
     return jsonify({"message": "XML data received successfully"})
 
 
