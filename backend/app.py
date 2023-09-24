ab6ffa80c59f547c13f9133993d226cc4be0e092
bitmaxx07 <84411099+bitmaxx07@users.noreply.github.com>
2023-09-24 17:11:11 +0200

    some new functions...

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,13 @@
+import os.path
+
 import xmltodict
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, render_template
 from flask_cors import CORS
 from main import *
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 CORS(app)
@@ -12,8 +18,10 @@ def index():
     return "Hello, World!"
 
 
-operation_name = "operation-operation1"
+# Always configure here!
+operation_name = "operation-op1"
 response = ""
+first_user_endpoint = ""
 
 
 @app.route('/upload-xml', methods=['POST'])
@@ -40,7 +48,7 @@ def upload_xml():
             if info["occupied"] == "false":
                 res_list.append(u)
 
-        response = "Available Users: \n"
+        response = "Available Users: "
 
         for u in res_list:
             info = get_user_info(xmltodict.parse(xml_data), u)
@@ -49,7 +57,11 @@ def upload_xml():
             response += u
             response += ", Endpoint of the user: "
             response += res_endpoint
-            response += "\n"
+            response += ". "
+
+        info = get_user_info(xmltodict.parse(xml_data), res_list[0])
+        global first_user_endpoint
+        first_user_endpoint = info["attribute"] + "/" + get_domain_by_operation(xml_data, operation_name)
 
         print(response)
 
@@ -58,8 +70,8 @@ def upload_xml():
 
     except Exception as e:
         print(e)
-        print(request.headers)
-        print(request.data)
+        # print(request.headers)
+        # print(request.data)
         '''print(request.content_type)
         return request.data'''
         error_response = make_response("Error: " + str(e), 400)
@@ -81,6 +93,73 @@ def get_response(op_name):
         return response
     else:
         return "Operation not found!"
+
+
+@app.route('/access/<string:op_name>', methods=['GET'])
+def get_resource(op_name):
+    """
+    This method returns all HTML elements of the endpoint of the first available user
+    :param op_name: operation name
+    :return: HTML elements of the first user's endpoint
+    """
+    if op_name == operation_name:
+        global first_user_endpoint
+        if not first_user_endpoint.startswith("https://") or not first_user_endpoint.startswith("http://"):
+            first_user_endpoint = "https://" + first_user_endpoint
+        r = requests.get(first_user_endpoint)
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.text, 'html.parser')
+            elements = soup.find_all(True)
+            res = ""
+            for e in elements:
+                res += str(e)
+            return res
+        else:
+            return "Failed to retrieve webpage, status code: " + str(r.status_code)
+        # return response
+    else:
+        return "Operation not found!"
+
+
+@app.route('/take-user/<string:username>', methods=['POST'])
+def take_user(username):
+    '''try:
+        # Trigger changeUserStatus(userName) in temp.html
+        # changeUserStatus(username)
+        javascript_call = f"changeUserStatus('{username}');"
+        javascript_path = os.path.join(os.path.abspath('../Whiteboard'), 'temp.html')
+        # return jsonify({"message": "User status changed successfully"})
+        return render_template(javascript_path, javascript_call=javascript_call)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500'''
+    try:
+        print("starting chrome driver...")
+        driver = webdriver.Chrome(executable_path="/Users/maqiaoming/Downloads/chromedriver-mac-x64/chromedriver")
+        print("ok for chrome driver")
+        driver.set_page_load_timeout(30)
+
+        driver.get('/Users/maqiaoming/WebstormProjects/RBAC_tool/Whiteboard/temp.html')
+
+        script = f"""
+        const userIndex = userList.findIndex(obj => obj.name === '{username}')
+        if (userIndex !== -1) {{
+          userList[userIndex].occupied = userList[userIndex].occupied === "true" ? "false" : "true";
+          console.log(userList[userIndex]);
+        }}
+        else {{
+          console.log("User not found!");
+        }}
+        """
+
+        driver.execute_script(script)
+        driver.quit()
+        return jsonify({"message": "User status changed successfully"})
+    except TimeoutException as e:
+        return "TimeoutException: Page load timed out."
+    except Exception as e:
+        error_message = "An error occurred: " + str(e)
+        print(error_message)
+        return error_message
 
 
 if __name__ == '__main__':
